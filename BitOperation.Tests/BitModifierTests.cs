62cf1dec38a89608307dde43480b0752300a2664
Sergey Struk <siarhey.struk@gmail.com>
2018-03-18 20:09:25 +0300

    first task added

---
+++ BitOperation.Tests/BitModifierTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BitOperation.Tests
+{
+    public class BitModifierTests
+    {
+        [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        [TestCase(8, 15, 3, 8, ExpectedResult = 120)]
+        public int InsertNumber_TestWithDiffValues(int a, int b, int i, int j)
+            => BitModifier.InsertNumber(a, b, i, j);
+
+        [Test]
+        public void InsertNumber_TestWithNegative() 
+            => Assert.Throws<ArgumentOutOfRangeException>(() => BitModifier.InsertNumber(8, 15, -4, -4));
+        [Test]
+        public void InsertNumber_TestEndLessThenStart()
+            => Assert.Throws<ArgumentOutOfRangeException>(() => BitModifier.InsertNumber(8, 15, 15, 8));
+        [Test]
+        public void InsertNumber_TestIndexOverFlow()
+            => Assert.Throws<ArgumentOutOfRangeException>(() => BitModifier.InsertNumber(8, 15, 34, 0));
+    }
+}
---
+++ BitOperation/BitModifier.cs
@@ -0,0 +1,93 @@
+﻿using System;
+namespace BitOperation
+{
+    /// <summary>
+    /// Static class which works with bites
+    /// </summary>
+    public static class BitModifier
+    {
+        /// <summary>
+        /// Inserts bytes of first number into second number
+        /// lefting second number's bytes on positions from start to end
+        /// </summary>
+        /// <param name="a"> 
+        /// First number
+        /// </param>
+        /// <param name="b">
+        /// Second Number
+        /// </param>
+        /// <param name="start">
+        /// Start index
+        /// </param>
+        /// <param name="end">
+        /// End index
+        /// </param>
+        /// <returns>
+        /// New int
+        /// </returns>
+        public static int InsertNumber(int a, int b, int start, int end)
+        {
+            if (start > end)
+            {
+                throw new ArgumentOutOfRangeException(nameof(start));
+            }
+
+            if (start < 0 || start > 31)
+            {
+                throw new ArgumentOutOfRangeException(nameof(start));
+            }
+
+            if (end < 0 || end > 31)
+            {
+                throw new ArgumentOutOfRangeException(nameof(end));
+            }
+
+            string firstNum = Convert.ToString(a, 2);
+            string secondNum = Convert.ToString(b, 2);
+            char[] firstNumChar = new char[32];
+            char[] secondNumChar = new char[32];
+            int countFirstLen = firstNum.Length - 1;
+            int countSecondLen = secondNum.Length - 1;
+            for (int i = 31; i >= 0; i--)
+            {
+                if (i > 31 - firstNum.Length)
+                {
+                    firstNumChar[i] = firstNum[countFirstLen];
+                    countFirstLen--;
+                }
+                else
+                {
+                    firstNumChar[i] = '0';
+                }
+
+                if (i > 31 - secondNum.Length)
+                {
+                    secondNumChar[i] = secondNum[countSecondLen];
+                    countSecondLen--;
+                }
+                else
+                {
+                    secondNumChar[i] = '0';
+                }
+            }
+
+            char[] result = new char[32];
+            int count = 31;
+
+            for (int i = 31; i >= 0; i--)
+            {
+                if (i >= 31 - end && i <= 31 - start)
+                {
+                    result[i] = secondNumChar[count];
+                    count--;
+                }
+                else
+                {
+                    result[i] = firstNumChar[i];
+                }
+            }
+
+            return Convert.ToInt32(new string(result), 2);
+        }
+    }
+}
